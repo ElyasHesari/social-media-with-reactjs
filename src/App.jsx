34b14c63da4270112fda6fa69f816eb27a11bfa6
Elyas Hesari <elyashesari@gmail.com>
2023-07-12 12:56:39 +0330

    Add Homepage components.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './Homepage';
+import './App.css'
+
+function App() {
+  return ( 
+    <div className='app'>
+      <Homepage />
+    </div>
+   );
+}
+
+export default App;
